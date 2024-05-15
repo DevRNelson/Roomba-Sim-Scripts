3fbb3eb8e2e950a59771478a61d1ce226f94dfea
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2024-05-15 03:01:37 -0400

    added a comment for deductionpersecond

--- TimerBattery.cs
+++ TimerBattery.cs
@@ -41,7 +41,7 @@ public class TimerBattery : MonoBehaviour
 
     private IEnumerator SprintBatteryLoss()
     {
-        float deductionPerSecond = 0.001f; // Constant deduction rate of 0.5 seconds per second
+        float deductionPerSecond = 0.001f; // Constant deduction rate of 0.001f (I believe the higher values are a bit too fast with how fast it drains, i feel this  and similar ranges to it is more reasonable for a level )
         float elapsedTime = 0f;
 
         while (PlayerMovement.isSprinting && GlobalVariables.currentTime > 0)
