c94a0c774c9dfc1a469fb78ab19d0762a976a86d
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2024-05-18 17:39:03 -0400

    Removed unused code and created back up file with code snippets way may use later

---
+++ BACK UP UNUSED SCRIPTS.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+//THIS UI SCRIPT EDIT TIES THE UI OF A BAR WITH A FILL AMOUNT TO THE TIME LEFT FOR E.G  A BATTERY BAR UI DISPLAY
+public class TimerScript : MonoBehaviour
+{
+    public Image cleanlinessBar;
+    public Text timerText;
+
+    public void SetTextboxText()
+    {
+        float displayTime = Mathf.Max(0, GlobalVariables.currentTime); // Ensure currentTime never goes below 0
+        int minutes = Mathf.FloorToInt(displayTime / 60);
+        int seconds = Mathf.FloorToInt(displayTime % 60);
+        string timeString = string.Format("{0:00}:{1:00}", minutes, seconds); // Display the time in the format Minutes:Seconds
+        UpdateTimerText(timeString);
+
+        // Calculate fill amount based on time
+        float fillAmount = displayTime / GlobalVariables.maxTime;
+        cleanlinessBar.fillAmount = fillAmount;
+    }
+
+    private void UpdateTimerText(string timeString)
+    {
+        timerText.text = timeString; // Update the timer text with the formatted time string
+    }
+}
